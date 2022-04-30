23de273da11d8f82749476479475e18fb2c40aba
Snapper <learningwithsnapper@gmail.com>
2022-04-29 22:38:12 -0400

    Added warnings to unused functions

--- app/operations/update.py
+++ app/operations/update.py
@@ -1,10 +1,13 @@
+import  warnings
+
+
 def city_state_zip_timestamp(db, city_state_zip_id):
-    return None
+    warnings.warn("Not implemented")
 
 
 def street_timestamp(db, street_id):
-    return None
+    warnings.warn("Not implemented")
 
 
 def address_timestamp(db, address_id):
-    return None
+    warnings.warn("Not implemented")
