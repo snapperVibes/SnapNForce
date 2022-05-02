4814b78e9ab5461566f24090004e91eab43c6e5b
Snapper <learningwithsnapper@gmail.com>
2022-05-02 12:38:28 -0400

    🔒 PG Pass

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+import warnings
 from collections import namedtuple
 from contextlib import contextmanager
 
@@ -7,28 +8,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.future import create_engine
 from sqlalchemy.orm import sessionmaker
 
-_Settings = namedtuple("_Settings", ["hostname", "port", "database", "username", "password"])
-
-# https://www.postgresql.org/docs/current/libpq-pgpass.html
-# def _parse_passwordfile(filename: str) -> list[_Settings]:
-#     with open(filename, "r") as f:
-#         original_lines = f.readlines()
-#     pattern = re.compile(r"(.*?)(#.*)?")
-#     lines = filter(lambda x: x == "", [re.split(pattern, line)[1].strip() for line in original_lines])
-#     pattern = re.compile(r"[^\\]:")
-#     return [_Settings(*re.split(pattern, line)[1:]) for line in lines]
-#
-# settings = _parse_passwordfile(PASSWORD_FILE)
-# if len(settings) > 1:
-#     raise RuntimeError("Multiple users are listed in the password file.")
-# settings = settings[0]
+from lib.vendor import pgpasslib
 
 
 _db_user = "sylvia"
-_db_password = "changeme"
 _host = "127.0.0.1"
 _port = "5432"
 _db_name = "cogdb"
+try:
+    _db_password = pgpasslib.getpass(host=_host, port=_port, dbname=_db_name, user=_db_user)
+except pgpasslib.PgPassException as err:
+    warnings.warn(
+        f"\nAn exception was handled while trying to read the password file:\n\t{type(err)}:{str(err)}.\nUsing development password instead.\nNote: you can specify the password file using the PGPASS environment variable."
+    )
+    _db_password = "changeme"
+
 _engine_params = f"postgresql+psycopg2://{_db_user}:{_db_password}@{_host}:{_port}/{_db_name}"
 _engine: Engine = create_engine(_engine_params, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=_engine)
