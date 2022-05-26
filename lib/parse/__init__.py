53d2eca10d69ad850c95265018526ea5cd9cce31
Snapper Vibes <learningwithsnapper@gmail.com>
2022-05-26 17:00:59 -0400

    exceptions

--- lib/parse/__init__.py
+++ lib/parse/__init__.py
@@ -10,6 +10,7 @@ __all__ = [
     "general_city_state_zip",
     "general_delivery_address_line",
     "general_city_state_zip",
+    "exceptions"
 ]
 from lib.parse.county_html import general_html_content, mortgage_html_content
 from lib.parse._mortgage import mortgage_delivery_address_line, mortgage_last_line
